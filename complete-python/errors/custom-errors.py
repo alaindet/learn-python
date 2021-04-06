5f6a2cb6ed908f85bcbaee2a88b55f3756cae5a5
Alain D'Ettorre <alain.det@gmail.com>
2021-04-06 15:03:53 +0200

    Add custom errors exercise

--- complete-python/errors/custom-errors.py
+++ complete-python/errors/custom-errors.py
@@ -1,3 +1,23 @@
+"""
+This is a module docstring
+This file is all about custom errors
+"""
+
+class CarTypeError(TypeError):
+    """
+    This is a class docstring
+    Exception raised when a Car instance is needed, but not provided
+    """
+
+    def __init__(self, message, code):
+        """
+        This is a method docstring
+        Just initialize a CarTypeError
+        """
+        super().__init__(f'Error code {code}: {message}')
+        self.code = code
+
+
 class Car:
     def __init__(self, maker, model):
         self.maker = maker
@@ -16,11 +36,10 @@ class Garage:
 
     def add_car(self, car):
         if not isinstance(car, Car):
-            errorType = car.__class__.__name__
-            raise TypeError(''.join([
-                'You must provide a Car to instance toGarage#add_car, ',
-                f'{errorType} given'
-            ]))
+            error_type = car.__class__.__name__
+            method = 'Garage#add_car'
+            error_message = f'{method} requires a Car istance, {error_type} given'
+            raise CarTypeError(error_message, 500)
 
 
 garage = Garage()
