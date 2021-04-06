5b4f33eacdc256ea20e30dafbff1947af1cd0c6f
Alain D'Ettorre <alain.det@gmail.com>
2021-04-06 07:59:15 +0200

    Add magic methods

--- complete-python/oop/magic-methods.py
+++ complete-python/oop/magic-methods.py
@@ -4,14 +4,41 @@ class Student:
 
 
 movies = ['Matrix', 'Finding Nemo']
-print(movies.__class__) # <class 'list'>
+# print(movies.__class__) # <class 'list'>
 
 
 class Garage:
+
     def __init__(self):
         self.cars = []
+
+    def __len__(self):
+        return len(self.cars)
+
+    def __getitem__(self, i):
+        return self.cars[i]
+
+    def __repr__(self):
+        """
+        Interacts with debugger
+        Gets called when printing the class, only if __str__ is not defined
+        """
+        return f'<Garage {self.cars}>'
+
+    def __str__(self):
+        return f'This garage has {len(self)} cars'
 
 my_garage = Garage()
 my_garage.cars.append('Car 1')
 my_garage.cars.append('Car 2')
-print(my_garage.cars)
+
+# Uses __len__
+# print(len(my_garage))
+# for car in my_garage:
+#     print(car)
+
+# Uses __getitem__
+# print(my_garage[0])
+
+# Uses __str__
+print(my_garage)
