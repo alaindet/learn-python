587bdb632746a9d52de0527956042c98777edc6d
Alain D'Ettorre <alain.det@gmail.com>
2021-04-19 14:56:45 +0200

    Add timing examples

---
+++ complete-python/advanced/time.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timezone, timedelta
+import time
+import timeit
+
+print('\nLocal time now')
+now = datetime.now()
+print(now) # OS-based local time
+
+print('\nUTC now')
+my_timezone = timezone.utc
+now_utc = datetime.now(my_timezone)
+print(now_utc) # UTC+0
+
+print('\nTime delta')
+tomorrow = now_utc + timedelta(days=1)
+print(tomorrow)
+
+print('\nTime string formatting')
+# Time format reference
+# https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+time_format = '%Y-%m-%d %H:%M'
+now_utc_formatted = now_utc.strftime(time_format)
+print(now_utc_formatted)
+
+# print('\nTime string parsing')
+# input_time_format = '%Y-%m-%d'
+# output_time_format = time_format
+# user_date = input('Enter the date in YYYY-mm-dd format: ')
+# parsed_user_date = datetime.strptime(user_date, input_time_format)
+# print(parsed_user_date.strftime(output_time_format))
+
+print('\nTracking timings via time package')
+
+def powers(limit):
+    return [x**2 for x in range(limit)]
+
+def measure_timing(func):
+    start_time = time.time()
+    func()
+    stop_time = time.time()
+    return stop_time - start_time
+
+LIMIT = 1_000_000
+
+func = lambda: powers(LIMIT)
+measured_timing = measure_timing(func)
+print(measured_timing)
+
+print('\nTracking timings via timeit package')
+print('\nTODO')
