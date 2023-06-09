f3cd750461898f7275a32b9c52374da6e6a2b544
Alain D'Ettorre <alain.det@gmail.com>
2023-06-09 17:35:59 +0200

    Add examples for variables, constants and comments

---
+++ courses/bootcamp_udemy/basics/constants.py
@@ -0,0 +1,10 @@
+"""
+There's no such thing as 'constants' in Python
+They're just variables
+"""
+
+PI = 3.1415
+SECONDS_IN_HOUR = 3600
+DAYS_IN_WEEK = 7
+
+PI = 3 # This is possible but terribly wrong
---
+++ courses/bootcamp_udemy/basics/variables.py
@@ -0,0 +1,6 @@
+KM_IN_MILES = 1.609
+
+miles = 10
+km = miles * KM_IN_MILES
+
+print(km)
