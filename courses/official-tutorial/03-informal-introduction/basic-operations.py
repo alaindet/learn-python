5bdb969e179c357a70430e02b50feb7d2978be3a
Alain D'Ettorre Aesys <alain.dettorre@aesys.tech>
2023-12-04 18:39:38 +0100

    Add examples for basic arithmetic operations

---
+++ courses/official-tutorial/03-informal-introduction/basic-operations.py
@@ -0,0 +1,7 @@
+print(2 + 2)         # 4
+print(50 - 5 * 6)    # 20
+print(2 + 3 * 4 / 3) # 6.0
+print(5 / 8)         # 0.625
+print(5 // 8)        # 0 <-- Integer division truncates, does not round
+print(16 % 3)        # 1
+print(2 ** 3)        # 8
