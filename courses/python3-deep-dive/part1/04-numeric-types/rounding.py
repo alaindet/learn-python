339b2fa41d8862bbac98a7bc93bb1d1165e2a2a6
Alain D'Ettorre Aesys <alain.dettorre@aesys.tech>
2024-05-22 12:21:48 +0200

    feat: add examples for rounding algorithms

--- courses/python3-deep-dive/part1/04-numeric-types/rounding.py
+++ courses/python3-deep-dive/part1/04-numeric-types/rounding.py
@@ -12,3 +12,27 @@ print(round(1.25, 1)) # 1.2 instead of 1.3!
 print(round(1.35, 1)) # 1.4 instead of 1.3!
 
 print(round(-1.25, 1)) # -1.2 instead of 1.3!
+
+# This is interesting
+# Giving negative numbers to the number of digits to round() rounds the integer
+# digits instead of the decimals. Here, we've got 20 and not 30 due to the
+# Banker's rounding algorithm
+print(round(25, -1)) # 20
+
+# How to force rounding "towards +infinity"? Like this (mind that this works
+# only with decimals)
+def roundTowardsPlusInfinity(n: float) -> int:
+    diff = -0.5 if n < 0 else 0.5
+    return int(n + diff)
+
+print(roundTowardsPlusInfinity(-10.4)) # -10
+print(roundTowardsPlusInfinity(-10.5)) # -11
+
+def sign(n):
+    return -1 if n < 0 else 1
+
+def roundAwayFromZero(n: float) -> int:
+    return sign(n) * int(abs(n) + 0.5)
+
+print(roundAwayFromZero(-10.4)) # -10
+print(roundAwayFromZero(-10.5)) # -11
