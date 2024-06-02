fba23ff431f0db5217ec1bc1fa4dcab302a8ccb7
Alain D'Ettorre <alain.det@gmail.com>
2024-06-02 08:04:17 +0200

    refactor: rps game

--- experiments/rock-paper-scissors/main.py
+++ experiments/rock-paper-scissors/main.py
@@ -1,11 +1,11 @@
 from contextlib import contextmanager
-from enum import StrEnum
+from enum import Enum
 from random import randint
-from typing import Optional
+from typing import Callable, Optional
 
 
 class Log:
-    """Thanks to https://stackoverflow.com/a/287944/5653974"""
+    """Utility class for colored logging output."""
     _sequence = {
         'blue': '\033[94m',
         'green': '\033[92m',
@@ -15,119 +15,121 @@ class Log:
     }
 
     @classmethod
-    def red(self, message: str) -> None:
-        self._print(message, self._sequence['red'])
+    def red(cls, message: str) -> None:
+        cls._print(message, cls._sequence['red'])
 
     @classmethod
-    def green(self, message: str) -> None:
-        self._print(message, self._sequence['green'])
+    def green(cls, message: str) -> None:
+        cls._print(message, cls._sequence['green'])
 
     @classmethod
-    def blue(self, message: str) -> None:
-        self._print(message, self._sequence['blue'])
+    def blue(cls, message: str) -> None:
+        cls._print(message, cls._sequence['blue'])
 
     @classmethod
-    def bold(self, message: str) -> None:
-        self._print(message, self._sequence['bold'])
+    def bold(cls, message: str) -> None:
+        cls._print(message, cls._sequence['bold'])
 
     @classmethod
-    def title(self, title: str) -> str:
+    def title(cls, title: str) -> str:
         return '\n'.join([title, '=' * len(title)])
 
     @classmethod
-    def _print(self, message: str, color: str) -> None:
-        end = self._sequence['end']
+    def _print(cls, message: str, color: str) -> None:
+        end = cls._sequence['end']
         print(f'{color}{message}{end}')
 
 
 class InvalidInputException(Exception):
+    """Exception raised for invalid input."""
     pass
 
 
 class QuitGameException(Exception):
+    """Exception raised to quit the game."""
     pass
 
 
-class Shape(StrEnum):
-    Rock = 'r'
-    Paper = 'p'
-    Scissors = 's'
+class Shape(Enum):
+    ROCK = 'r'
+    PAPER = 'p'
+    SCISSORS = 's'
 
     @classmethod
-    def display(self, shape: Optional[str]) -> str:
+    def display(cls, shape: Optional[str]) -> str:
         match shape:
-            case self.Rock:
+            case cls.ROCK:
                 return 'Rock'
-            case self.Paper:
+            case cls.PAPER:
                 return 'Paper'
-            case self.Scissors:
+            case cls.SCISSORS:
                 return 'Scissors'
             case _:
                 return '(None)'
 
 
 class Player:
-    name: str
-    score = 0
-    last_move: Optional[Shape] = None
-
-    def __init__(self, name: str):
+    def __init__(self, name: str, logger: Callable[[str], None]):
         self.name = name
+        self.score = 0
+        self.logger = logger
+        self.last_move: Optional[Shape] = None
 
-    def display(self) -> str:
-        return f'{self.name} = {self.score}'
+    def log_last_move(self) -> None:
+        last_move = Shape.display(self.last_move)
+        self.logger(f'{self.name} played: {last_move}')
 
-    def display_move(self) -> str:
-        return Shape.display(self.last_move)
+    def log_win(self) -> None:
+        self.logger(f'[OUTCOME] {self.name} won!')
+
+    def log_score(self) -> None:
+        self.logger(f'{self.name} score = {self.score}')
 
     def ask_move(self) -> None:
         self.last_move = self._read_move()
 
     def _read_move(self) -> Shape:
         while True:
-            print('\n'.join([
-                'Type a letter to select a shape (r=Rock, p=Paper, s=Scissors).',
-                'Type q to quit',
-            ]))
+            print('\nType a letter to select a shape (r=Rock, p=Paper, s=Scissors).')
+            print('Type q to quit')
 
             try:
                 user_input = input('> ').lower()
 
-                if user_input in ['q', 'quit']:
+                if user_input == 'q':
                     raise QuitGameException()
 
-                if user_input not in Shape:
+                if user_input not in [shape.value for shape in Shape]:
                     raise InvalidInputException()
 
                 return Shape(user_input)
 
-            except (InvalidInputException, ValueError):
+            except InvalidInputException:
                 print('ERROR: Invalid input. Try again')
 
-    def random_move(self) -> Shape:
-        """TODO: Improve"""
+    def random_move(self) -> None:
+        """Assign a random move to the player."""
         n = randint(1, 3)
         match n:
             case 1:
-                self.last_move = Shape.Rock
+                self.last_move = Shape.ROCK
             case 2:
-                self.last_move = Shape.Paper
+                self.last_move = Shape.PAPER
             case 3:
-                self.last_move = Shape.Scissors
+                self.last_move = Shape.SCISSORS
 
 
 def check_winner(player1: Player, player2: Player) -> Optional[Player]:
-
-    p1 = player1.last_move
-    p2 = player2.last_move
+    """Determine the winner between two players."""
+    p1, p2 = player1.last_move, player2.last_move
 
     if p1 == p2:
         return None
 
     if (
-        (p1 == Shape.Rock and p2 == Shape.Scissors) or
-        (p1 == Shape.Paper and p2 == Shape.Rock) or
-        (p1 == Shape.Scissors and p2 == Shape.Paper)
+        (p1 == Shape.ROCK and p2 == Shape.SCISSORS) or
+        (p1 == Shape.PAPER and p2 == Shape.ROCK) or
+        (p1 == Shape.SCISSORS and p2 == Shape.PAPER)
     ):
         return player1
 
@@ -137,49 +139,51 @@ def check_winner(player1: Player, player2: Player) -> Optional[Player]:
 @contextmanager
 def game_context():
     Log.bold('\n' + Log.title('Rock Paper Scissors'))
-
     try:
         yield
-    except KeyboardInterrupt:
-        pass
-    except QuitGameException:
+    except (KeyboardInterrupt, QuitGameException):
         print('You quit the game')
     finally:
         print('\nIt was nice playing with you. Bye!')
 
 
 def turns():
-    i = 1
+    """Generator function to track game turns."""
+    turn_number = 1
     while True:
-        yield i
-        i += 1
+        yield turn_number
+        turn_number += 1
 
 
 def main() -> None:
+    """Main function to start the game."""
     with game_context():
-        player1 = Player('User')
-        player2 = Player('CPU')
+        player1 = Player('User', Log.blue)
+        player2 = Player('CPU', Log.green)
+        draws = 0
 
         for turn in turns():
-
             Log.bold('\n' + Log.title(f'Turn #{turn}'))
 
             player1.ask_move()
-            Log.blue(f'{player1.name} played: {player1.display_move()}')
+            player1.log_last_move()
 
             player2.random_move()
-            Log.green(f'{player2.name} played: {player2.display_move()}')
+            player2.log_last_move()
 
+            print('――――――')
             winner = check_winner(player1, player2)
-
             if winner:
                 winner.score += 1
-                logger = Log.blue if winner is player1 else Log.green
-                logger(f'Outcome: {winner.name} won!')
+                winner.log_win()
             else:
-                Log.bold('Outcome: Draw!')
+                Log.bold('[OUTCOME]: Draw!')
+                draws += 1
+            print('――――――')
 
-            print(f'Score: {player1.display()}, {player2.display()}')
+            player1.log_score()
+            player2.log_score()
+            print(f'Draws = {draws}')
 
 
 if __name__ == '__main__':
