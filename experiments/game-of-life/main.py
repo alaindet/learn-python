b9902e8e32054ee426dc1ad937bc9c129aa9c6aa
Alain D'Ettorre <alain.det@gmail.com>
2024-06-02 10:58:33 +0200

    WIP: add Conway's Game of Life implementation

--- experiments/game-of-life/main.py
+++ experiments/game-of-life/main.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 import time
 import random
 import os
@@ -24,26 +25,106 @@ def print_state(state: GameState, size=20) -> None:
         print(''.join([ALIVE if cell else DEAD for cell in chunk]))
 
 
+def get_state_from_string(state_string: str, alive: str, dead: str) -> GameState:
+    state: List[bool] = []
+    for char in state_string.strip():
+        if char == alive:
+            state.append(True)
+        elif char == dead:
+            state.append(False)
+    return state
+
+
+def get_random_state(size: int) -> GameState:
+    return [random.getrandbits(1) for i in range(size * size)]
+
+
+def get_empty_state(size: int) -> GameState:
+    return [False for i in range(size * size)]
+
+
+def get_neighbors_indices(size: int, index: int) -> List[int]:
+    """Calculates valid indices of neighbors excluding outbound indices when
+    the cell is on the grid's edge. Here, t=top, tl=top left, b = bottom, etc."""
+
+    t = index - size
+    tl, tr = t - 1, t + 1
+    b = index + size
+    bl, br = b - 1, b + 1
+    l, r = index - 1, index + 1
+
+    first_index = 0
+    last_index = (size * size) - 1
+
+    on_top_edge = t < first_index
+    on_bottom_edge = b > last_index
+    on_left_edge = index % size == 0
+    on_right_edge = (index + 1) % size == 0
+    invalid_index = -1
+
+    if on_left_edge:
+        tl, l, bl = invalid_index, invalid_index, invalid_index
+
+    if on_right_edge:
+        tr, r, br = invalid_index, invalid_index, invalid_index
+
+    if on_top_edge:
+        tl, t, tr = invalid_index, invalid_index, invalid_index
+
+    if on_bottom_edge:
+        bl, b, br = invalid_index, invalid_index, invalid_index
+
+    indices = [tl, t, tr, r, br, b, bl, l]
+    return [index for index in indices if index != invalid_index]
+
+
+def count_neighbors(state: GameState, size: int, index: int) -> int:
+    count = 0
+    for index in get_neighbors_indices(size, index):
+        if state[index]:
+            count += 1
+    return count
+
+
+def next_cell_state(state: GameState, size: int, index: int) -> bool:
+    is_alive = state[index]
+    neighbors = count_neighbors(state, size, index)
+
+    # A dead cell comes alive
+    if not is_alive and neighbors == 3:
+        return True
+
+    # A live cell dies for underpopulation
+    if neighbors < 2:
+        return False
+
+    # A live cell dies for overpopulation
+    if neighbors > 3:
+        return False
+
+    # A live cell stays alive
+    return True
+
+
 def calculate_next_state(state: GameState, size=20) -> GameState:
-    """TODO"""
-    return state
-
-
-def get_random_state(grid_size: int) -> GameState:
-    state: GameState = []
-    for i in range(grid_size * grid_size):
-        state.append(random.getrandbits(1))
-    return state
+    return [next_cell_state(state, size, i) for i, is_alive in enumerate(state)]
 
 
 def print_generation(generation: int, state: GameState, size: int) -> None:
     clear_output()
     print(f'Generation #{generation}')
     print_state(state, size)
-    time.sleep(0.5)
 
 
 def game_of_life(*, generations=10, size=20, t=10, state: GameState) -> None:
+
+    # print_generation(0, state, size)
+
+    # state = calculate_next_state(state, size)
+    # print_generation(1, state, size)
+
+    # state = calculate_next_state(state, size)
+    # print_generation(2, state, size)
 
     interval = t/generations
 
@@ -63,7 +144,28 @@ def game_of_life_random_state(**kwargs) -> None:
 
 
 def main() -> None:
-    game_of_life_random_state(generations=10, size=20, t=10)
+
+    serialized_state = dedent("""
+    ................
+    ................
+    ................
+    ................
+    ................
+    .......xxx......
+    ................
+    .....x.....x....
+    .....x.....x....
+    .....x.....x....
+    ................
+    .......xxx......
+    ................
+    ................
+    ................
+    ................
+    """)
+
+    state = get_state_from_string(serialized_state, alive='x', dead='.')
+    game_of_life(generations=20, size=16, t=1, state=state)
 
 
 if __name__ == '__main__':
