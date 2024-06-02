be2fb670649585af9bb2a20a2d9152d9e4851eb3
Alain D'Ettorre <alain.det@gmail.com>
2024-06-02 17:13:45 +0200

    add another example to filter() examples

--- courses/python3-deep-dive/part1/06-first-class-functions/filter.py
+++ courses/python3-deep-dive/part1/06-first-class-functions/filter.py
@@ -9,3 +9,12 @@ nums = [1, 2, 3, 4]
 # <filter object at 0x0000025490EBE650>
 print(filter(lambda n: n % 2 == 0, nums))
 print(list(filter(lambda n: n % 2 == 0, nums)))  # [2, 4]
+
+# If you pass no function to filter, it just checks for truthyness
+junk = [1, 0, 4, 'a', '', None, True, False]
+filtered1 = list(filter(None, junk))
+filtered2 = list(filter(lambda x: bool(x), junk))
+
+print(filtered1)  # [1, 4, 'a', True]
+print(filtered2)  # [1, 4, 'a', True]
+print(filtered1 == filtered2)  # True
