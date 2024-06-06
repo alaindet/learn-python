6ac6b9ca35d16bb12fb6b1f8a767068d5cd29ff1
Alain D'Ettorre <alain.det@gmail.com>
2024-06-06 20:34:17 +0200

    add example @timed decorator

--- courses/python3-deep-dive/part1/07-scopes-closures-decorators/decorators_2.py
+++ courses/python3-deep-dive/part1/07-scopes-closures-decorators/decorators_2.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def timed(fn):
     from time import perf_counter
     from functools import wraps
@@ -15,13 +18,43 @@ def timed(fn):
 
     return inner
 
+
 def _fibo(n: int) -> int:
     if n <= 2:
         return 1
     return _fibo(n - 1) + _fibo(n - 2)
 
+
 @timed
 def fibo(n: int) -> int:
     return _fibo(n)
 
-print(fibo(30))
+
+@timed
+def fibo_loop(n: int) -> int:
+    a, b = 1, 1
+    for i in range(3, n + 1):
+        a, b = b, a + b
+    return b
+
+
+# Implementing Fibonacci's sequence with a loop in much faster (20'000 times!) than
+# the bare recursion implementation
+print(fibo(30))  # 832040, took 0.127641 seconds
+print(fibo_loop(30))  # 832040, took 0.000011 seconds
+
+
+@timed
+def fibo_reduce(n: int) -> int:
+    initial = (1, 0)
+    dummy = range(n-1)
+    result = reduce(
+        lambda prev, n: (prev[0] + prev[1], prev[0]),
+        dummy,
+        initial
+    )
+    return result[0]
+
+
+# It's a little slower than
+print(fibo_reduce(30))  # 832040, took 0.000018 seconds
